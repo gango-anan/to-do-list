cddbb745db893dd11927067e34a666db76b0d1b5
Jocyline Magero <jmagero2017@gmail.com>
2021-05-06 23:30:13 +0300

    Create project constructor && submit listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const projectsContainer = document.querySelector('.projects__list');
+const formProject = document.querySelector('.projects__form');
+const projectInput = document.querySelector('.projects__form__input')
 
-let projects = ['General', 'Coding', 'Movie Night'];
+let projects = [{name:"General",tasks: []}];
 
 // Utility Functions
 function renderProjects() {
@@ -8,13 +10,32 @@ function renderProjects() {
     const projectItem = document.createElement('li');
     projectItem.classList.add('projects__item');
     projectItem.setAttribute('id', `1`);
-    projectItem.innerText = project;
+    projectItem.innerText = project.name;
     projectsContainer.appendChild(projectItem);
   })
 }
 
+function Project (name, tasks =[]) {
+    this.name = name
+    this.tasks = tasks
+}
+
+
+
+function createProject (){
+  
+}
 
 // Event Listeners
+
+formProject.addEventListener('submit',(e) => {
+  e.preventDefault()
+  const newProjectvalue = projectInput.value;
+  const newProject = new Project(newProjectvalue)
+  projects.push(newProject)
+  renderProjects()
+})
+
 
 
 
